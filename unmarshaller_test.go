3e9c25ff4cac6335469b0a5bd194c52aa21c3b9e
AlexSkilled <olegthander@yandex.ru>
2025-06-14 13:54:34 +0300

    [master] snake test

--- unmarshaller_test.go
+++ unmarshaller_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestUnmarshalToMap(t *testing.T) {
@@ -23,9 +24,23 @@ func TestUnmarshalToMap(t *testing.T) {
 }
 
 func TestUnmarshalMatreshkaToMap(t *testing.T) {
-	ns := ParseToNodes(matreshkaDotEnv)
+	ns := ParseToNodes(simpleObjectDotEnv)
 
 	targetMap := map[string]any{}
 	err := UnmarshalWithNodes(ns, targetMap, WithSnakeUnmarshal())
 	require.NoError(t, err)
+
+	expected := map[string]any{
+		"pointer_value":  "31",
+		"root_int_value": "3",
+		"child_obj_value": map[string]any{
+			"bool_value":   "true",
+			"string_value": "12",
+		},
+	}
+
+	require.Equal(t, expected, targetMap)
+
+	r, _ := yaml.Marshal(expected)
+	print(string(r))
 }
