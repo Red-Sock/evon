6eebe9d93a28fc84a8a3db15b9c649b15c23cba8
AlexSkilled <olegthander@yandex.ru>
2025-11-16 18:48:37 +0300

    [master] work with float values

--- marshaller.go
+++ marshaller.go
@@ -78,8 +78,12 @@ func (m marshaller) marshal(prefix string, ref reflect.Value) (n *Node, err erro
 
 	case reflect.Map:
 		n, err = marshalMap(prefix, ref)
-	case reflect.String, reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+	case reflect.String,
+		reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64,
+		reflect.Uintptr:
 		n = &Node{
 			Name:  prefix,
 			Value: ref.Interface(),
